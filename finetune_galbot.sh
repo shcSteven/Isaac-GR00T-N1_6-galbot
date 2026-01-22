f3b43b607381e0e242223624b5202636c3d0d639
shcSteven <haochens@nvidia.com>
2026-01-22 15:22:11 +0800

    for mingxin to launch finetuning

--- finetune_galbot.sh
+++ finetune_galbot.sh
@@ -2,7 +2,7 @@
 set -x -e
 
 export NUM_GPUS=2
-TASK_NAME="task6_01120119_merged"
+TASK_NAME="task3_01210122_merged"
 
 # Path configuration
 DATASET_PATH="/localhome/local-haochens/data/galbot_lerobot_dataset/${TASK_NAME}"
@@ -27,4 +27,5 @@ CUDA_VISIBLE_DEVICES=0,1 uv run torchrun --nproc_per_node=${NUM_GPUS} ./gr00t/ex
     --global_batch_size 32 \
     --color_jitter_params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
     --dataloader_num_workers 2 \
+    --state_dropout_prob 0.1 \
     --tune_visual
